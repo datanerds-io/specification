dd71b3ba8e7a26059b83825cc93a5c666b3d39ec
Lars Pfannenschmidt <lars@d8a.wtf>
2016-03-06 15:39:34 +0100

    Added some JavaDoc

--- specification/src/main/java/io/datanerds/specification/Specification.java
+++ specification/src/main/java/io/datanerds/specification/Specification.java
@@ -2,12 +2,36 @@ package io.datanerds.specification;
 
 import java.util.Objects;
 
+/**
+ * This interface is an Java 8 implementation of the Specification Pattern described by Eric Evans and Martin Fowler
+ * <a href="http://martinfowler.com/apsupp/spec.pdf">here</a>. The difference in this implementation is the possibility
+ * to pass multiple specifications to the {@link Specification#and(Specification[])} and
+ * {@link Specification#or(Specification[])} methods.
+ *
+ * @param <T> The type you want to "compare"
+ */
 public interface Specification<T> {
 
     boolean isSatisfiedBy(T t);
 
+    /**
+     * This method will negate a given specification.
+     *
+     * @return the negated return value of the original specification
+     */
+    default Specification<T> not() {
+        return spec -> !isSatisfiedBy(spec);
+    }
+
+    /**
+     * And specification for a list of specifications.
+     *
+     * @param others specifications for comparison
+     * @return a TRUE specification if it is satisfied by all passed specifications,
+     * FALSE otherwise
+     */
     default Specification<T> and(Specification<T>... others) {
-        Objects.requireNonNull(others, "Specification needs to be set");
+        Objects.requireNonNull(others, "Specification(s) needs to be set");
         return spec -> {
             boolean result = true;
             for (Specification<T> other : others) {
@@ -17,8 +41,16 @@ public interface Specification<T> {
         };
     }
 
+    /**
+     * Or specification for a list of specifications.
+     *
+     * @param others specifications for comparison
+     * @return a TRUE specification if it is satisfied by one of the passed specifications,
+     * FALSE otherwise
+     */
+
     default Specification<T> or(Specification<T>... others) {
-        Objects.requireNonNull(others, "Specification needs to be set");
+        Objects.requireNonNull(others, "Specification(s) needs to be set");
         return spec -> {
             boolean result = false;
             for (Specification<T> other : others) {
@@ -26,9 +58,5 @@ public interface Specification<T> {
             }
             return result;
         };
-    }
-
-    default Specification<T> not() {
-        return spec -> !isSatisfiedBy(spec);
     }
 }
