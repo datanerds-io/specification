5a10696b6d34ce7e470dd0783f8f42320a3189e4
Daniel Hiller <Daniel_Hiller@intuit.com>
2016-12-30 20:24:43 +0100

    Converted test into parameterized

--- specification/src/test/java/io/datanerds/specification/SpecificationTest.java
+++ specification/src/test/java/io/datanerds/specification/SpecificationTest.java
@@ -1,192 +1,79 @@
 package io.datanerds.specification;
 
+import java.util.Collection;
+
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static java.util.Arrays.asList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(Enclosed.class)
+@RunWith(Parameterized.class)
 public class SpecificationTest {
 
-    public abstract static class TestBase {
+    private static final Specification<String> containsDatanerds = spec -> spec.contains("datanerds");
+    private static final Specification<String> isLongerThan9 = spec -> spec.length() > 9;
+    private static final Specification<String> isShorterThan15 = spec -> spec.length() < 15;
+    private static final Specification<String> startsWithD = spec -> spec.length() > 0 && spec.startsWith("d");
+    private static final Specification<String> containsDatanerdsAndIsShorterThan15 =
+            containsDatanerds.and(isShorterThan15);
+    private static final Specification<String> contDNAndIsNotLTh9AndNotStartsWD =
+            containsDatanerds.and(isLongerThan9.not()).and(startsWithD.not());
+    private static final Specification<String> contDNOrIsNotLTh9OrNotStartsWD =
+            containsDatanerds.not().or(isLongerThan9.not()).or(startsWithD.not());
 
-        protected final Specification<String> containsDatanerds = spec -> spec.contains("datanerds");
-        protected final Specification<String> isLongerThan9 = spec -> spec.length() > 9;
-        protected final Specification<String> isShorterThan15 = spec -> spec.length() < 15;
-        protected final Specification<String> startsWithD = spec -> spec.length() > 0 && spec.startsWith("d");
+    @Parameterized.Parameters(name = "{index}: spec using value {1} should satisfy: {2}")
+    public static Collection<Object[]> data() {
+        return asList(new Object[][]{
+                        {containsDatanerds.not(), "datafreaks", true},
+                        {containsDatanerds.not(), "datanerds", false},
+                        {isLongerThan9.not(), "datanerdss", false},
+                        {isLongerThan9.not(), "data", true},
+                        {startsWithD.not(), "datanerds", false},
+                        {startsWithD.not(), "atanerds", true},
+                        {containsDatanerds.and(startsWithD), "datanerds", true},
+                        {containsDatanerds.and(startsWithD), "atanerds", false},
+                        {containsDatanerds.and(startsWithD, isLongerThan9, isShorterThan15), "datanerdsss", true},
+                        {containsDatanerds.and(startsWithD, isLongerThan9, isShorterThan15),
+                                "datanerdsdatanerds", false},
+                        {containsDatanerds.or(startsWithD), "datafreaks", true},
+                        {containsDatanerds.or(startsWithD), "atafreaks", false},
+                        {startsWithD.or(isLongerThan9), "1234567890", true},
+                        {startsWithD.or(isLongerThan9), "1", false},
+                        {containsDatanerds.or(startsWithD, isLongerThan9), "1234567890", true},
+                        {containsDatanerds.or(startsWithD, isLongerThan9, isShorterThan15), "1234567890", true},
+                        {containsDatanerdsAndIsShorterThan15.or(startsWithD), "d", true},
+                        {containsDatanerdsAndIsShorterThan15.or(startsWithD), "d", true},
+                        {containsDatanerdsAndIsShorterThan15.or(startsWithD), "datafreaks", true},
+                        {containsDatanerdsAndIsShorterThan15.or(startsWithD), "atafreaksdatafreaks", false},
+                        {containsDatanerdsAndIsShorterThan15.not().or(startsWithD), "atafreaksdatafreaks", true},
+                        {contDNAndIsNotLTh9AndNotStartsWD, "datanerdsdatanerds", false},
+                        {contDNAndIsNotLTh9AndNotStartsWD, "atanerdsdatanerds", false},
+                        {contDNAndIsNotLTh9AndNotStartsWD, "datanerds", false},
+                        {contDNOrIsNotLTh9OrNotStartsWD, "datanerdsdatanerds", false},
+                        {contDNOrIsNotLTh9OrNotStartsWD, "atanerdsdatanerds", true},
+                        {contDNOrIsNotLTh9OrNotStartsWD, "d", true},
+                        {contDNOrIsNotLTh9OrNotStartsWD, "12345678", true},
 
+                }
+        );
     }
 
-    public static class Not extends TestBase {
+    @Parameterized.Parameter
+    public Specification<String> spec;
 
-        @Test
-        public void not() {
-            assertThat(containsDatanerds.not().isSatisfiedBy("datafreaks"), is(true));
-        }
+    @Parameterized.Parameter(value = 1)
+    public String value;
 
-        @Test
-        public void not1() {
-            assertThat(containsDatanerds.not().isSatisfiedBy("datanerds"), is(false));
-        }
+    @Parameterized.Parameter(value = 2)
+    public boolean expected;
 
-        @Test
-        public void not2() {
-            assertThat(isLongerThan9.not().isSatisfiedBy("datanerdss"), is(false));
-        }
-
-        @Test
-        public void not3() {
-            assertThat(isLongerThan9.not().isSatisfiedBy("data"), is(true));
-        }
-
-        @Test
-        public void not4() {
-            assertThat(startsWithD.not().isSatisfiedBy("datanerds"), is(false));
-        }
-
-        @Test
-        public void not5() {
-            assertThat(startsWithD.not().isSatisfiedBy("atanerds"), is(true));
-        }
-
-    }
-
-    public static class And extends TestBase {
-
-        @Test
-        public void and() {
-            assertThat(containsDatanerds.and(startsWithD).isSatisfiedBy("datanerds"), is(true));
-        }
-
-        @Test
-        public void and1() {
-            assertThat(containsDatanerds.and(startsWithD).isSatisfiedBy("atanerds"), is(false));
-        }
-
-        @Test
-        public void and2() {
-            assertThat(containsDatanerds.and(startsWithD, isLongerThan9, isShorterThan15).isSatisfiedBy("datanerdsss"),
-                    is(true));
-        }
-
-        @Test
-        public void and3() {
-            assertThat(
-                    containsDatanerds.and(startsWithD, isLongerThan9, isShorterThan15).isSatisfiedBy
-                            ("datanerdsdatanerds"),
-                    is(false));
-        }
-
-    }
-
-    public static class Or extends TestBase {
-
-        @Test
-        public void or() {
-            assertThat(containsDatanerds.or(startsWithD).isSatisfiedBy("datafreaks"), is(true));
-        }
-
-        @Test
-        public void or1() {
-            assertThat(containsDatanerds.or(startsWithD).isSatisfiedBy("atafreaks"), is(false));
-        }
-
-        @Test
-        public void or2() {
-            assertThat(startsWithD.or(isLongerThan9).isSatisfiedBy("1234567890"), is(true));
-        }
-
-        @Test
-        public void or3() {
-            assertThat(startsWithD.or(isLongerThan9).isSatisfiedBy("1"), is(false));
-        }
-
-        @Test
-        public void or4() {
-            assertThat(containsDatanerds.or(startsWithD, isLongerThan9).isSatisfiedBy("1234567890"), is(true));
-        }
-
-        @Test
-        public void or5() {
-            assertThat(containsDatanerds.or(startsWithD, isLongerThan9, isShorterThan15).isSatisfiedBy("1234567890"),
-                    is(true));
-        }
-
-    }
-
-    public static class AndOr extends TestBase {
-
-        Specification<String> and = containsDatanerds.and(isShorterThan15);
-
-        @Test
-        public void andOr() {
-            assertThat(and.or(startsWithD).isSatisfiedBy("d"), is(true));
-        }
-
-        @Test
-        public void andOr1() {
-            assertThat(and.or(startsWithD).isSatisfiedBy("d"), is(true));
-        }
-
-        @Test
-        public void andOr2() {
-            assertThat(and.or(startsWithD).isSatisfiedBy("datafreaks"), is(true));
-        }
-
-        @Test
-        public void andOr3() {
-            assertThat(and.or(startsWithD).isSatisfiedBy("atafreaksdatafreaks"), is(false));
-        }
-
-        @Test
-        public void andOr4() {
-            assertThat(and.not().or(startsWithD).isSatisfiedBy("atafreaksdatafreaks"), is(true));
-        }
-
-    }
-
-    public static class Complex extends TestBase {
-
-        Specification<String> complex1 = containsDatanerds.and(isLongerThan9.not()).and(startsWithD.not());
-        Specification<String> complex2 = containsDatanerds.not().or(isLongerThan9.not()).or(startsWithD.not());
-
-        @Test
-        public void complex1() {
-            assertThat(complex1.isSatisfiedBy("datanerdsdatanerds"), is(false));
-        }
-
-        @Test
-        public void complex11() {
-            assertThat(complex1.isSatisfiedBy("atanerdsdatanerds"), is(false));
-        }
-
-        @Test
-        public void complex12() {
-            assertThat(complex1.isSatisfiedBy("datanerds"), is(false));
-        }
-
-        @Test
-        public void complex2() {
-            assertThat(complex2.isSatisfiedBy("datanerdsdatanerds"), is(false));
-        }
-
-        @Test
-        public void complex21() {
-            assertThat(complex2.isSatisfiedBy("atanerdsdatanerds"), is(true));
-        }
-
-        @Test
-        public void complex22() {
-            assertThat(complex2.isSatisfiedBy("d"), is(true));
-        }
-
-        @Test
-        public void complex23() {
-            assertThat(complex2.isSatisfiedBy("12345678"), is(true));
-        }
-
+    @Test
+    public void satisfies() {
+        assertThat(spec.isSatisfiedBy(value), is(expected));
     }
 
 }
